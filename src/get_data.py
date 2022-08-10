873207363ba6634273d755afd90042becc7632c2
Neel7317 <65647192+Neel7317@users.noreply.github.com>
2022-08-10 16:12:47 +0530

    stage1 completed

--- src/get_data.py
+++ src/get_data.py
@@ -16,7 +16,6 @@ def get_data(config_path):
     # print(config)
     data_path = config["data_source"]["s3_source"]
     df = pd.read_csv(data_path, sep=",", encoding='utf-8')
-    print(df.head())
     return df
 
 
